0aa750d76349c5c8605de8541acbc9269f646383
Matoy7 <yotam.eliraz@gmail.com>
2026-08-09 11:59:08 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useRef } from 'react'
 import Homepage from '@/imports/Homepage/index'
 import CanvasFrame from './CanvasFrame'
+import { useInteractionLayer } from './interactions/useInteractionLayer'
 
 export default function App() {
+  const designRef = useRef<HTMLDivElement>(null)
+
+  // Applies the site-wide interaction system on top of the untouched design.
+  useInteractionLayer(designRef)
+
   return (
     <CanvasFrame>
-      <Homepage />
+      <div ref={designRef}>
+        <Homepage />
+      </div>
     </CanvasFrame>
   )
 }
